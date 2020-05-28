ca35116dc007e9cce53943c18c98334a21a5178a
jhart3000 <j.hart4000@gmail.com>
2020-05-28 17:24:40 +0100

    first commit

---
+++ src/test/java/com/game/snakeeyes/SnakeeyesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.snakeeyes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeeyesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
